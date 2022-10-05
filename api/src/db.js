591575a89efb103cdf7c946d1d254fd32e58406d
Artur-0 <romeroarturo597@gmail.com>
2022-10-05 15:11:43 -0300

    db

--- api/src/db.js
+++ api/src/db.js
@@ -3,17 +3,17 @@ const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
 // const sequelize = require("sequelize");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, PORT } = process.env;
+const { PGUSER, PGPASSWORD, PGHOST, PGNAME, PGPORT } = process.env;
 
 let sequelize =
   process.env.NODE_ENV === "production"
     ? new Sequelize({
-        database: DB_NAME,
+        database: PGNAME,
         dialect: "postgres",
-        host: DB_HOST,
-        port: PORT,
-        username: DB_USER,
-        password: DB_PASSWORD,
+        host: PGHOST,
+        port: PGPORT,
+        username: PGUSER,
+        password: PGPASSWORD,
         pool: {
           max: 3,
           min: 1,
@@ -28,7 +28,7 @@ let sequelize =
         },
         ssl: true,
       })
-    : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`, {
+    : new Sequelize(`postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/pokemon`, {
         logging: false, // set to console.log to see the raw SQL queries
         native: false, // lets Sequelize know we can use pg-native for ~30% more speed
       });
