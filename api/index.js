dc41556737c490bc21f8fbf28067cafec3e30998
Artur-0 <romeroarturo597@gmail.com>
2022-11-14 17:01:07 -0300

    clean db

--- api/index.js
+++ api/index.js
@@ -21,8 +21,8 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
-  //change for PGPORT when using locally
+conn.sync({ force: true }).then(() => {
+  //change for PGPORT when using locally and PORT for deploy
   server.listen(process.env.PORT, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
